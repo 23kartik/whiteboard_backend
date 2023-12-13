023956e1e8bc97b0b4fd1c4294c500872233a87d
23kartik <kartik23tiwari@gmai.com>
2023-12-13 13:19:02 +0530

    user auth completed

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+
+const userSchema=mongoose.Schema(
+    {
+
+
+username:{
+    type:String,
+    required:[true,"Please add username"],
+
+},
+
+email:{
+type:String,
+required:[true,"Please add the user email"],
+unique:[true,"Email address already exists"]
+},
+
+password:{
+type:String,
+required:[true,"Please add the user password"],
+},
+
+},
+{
+    timestamps:true,
+
+});
+
+module.exports=mongoose.model("User",userSchema);
---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/userController");
+// const validateToken = require("../middleware/validateTokenHandler");
+const router = express.Router();
+
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+module.exports = router;

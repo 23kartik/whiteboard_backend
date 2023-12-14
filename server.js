d83b2d0f3a58887526a21c3813499b62990e6e39
23kartik <kartik23tiwari@gmai.com>
2023-12-14 13:39:11 +0530

    reverted some changing

--- server.js
+++ server.js
@@ -9,29 +9,27 @@ const app=express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(bodyParser.json());
 app.use(express.json());
-mongoose.connect('mongodb+srv://23kartik:23kartik@cluster0.py7kx0e.mongodb.net/user_cred?retryWrites=true&w=majority');
 
-// Socket.io events
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle whiteboard events (we'll add this later)
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+mongoose.connect('mongodb+srv://23kartik:23kartik@cluster0.py7kx0e.mongodb.net/user_cred?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
   });
-});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the collaborative whiteboard!');
   });
 
-
   
   // Routes
 app.use("/api/users",require("./routes/userRoutes"));
+
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

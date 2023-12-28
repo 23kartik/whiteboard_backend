10be42bdd71695a58f3f1639d000d55d6c0506da
23kartik <kartik23900@gmail.com>
2023-12-29 03:34:21 +0530

    backend works good, emit is working fine

--- controllers/drawingController.js
+++ controllers/drawingController.js
@@ -11,13 +11,26 @@ const initializeDrawing = (server) => {
   io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('draw', (data) => {
-      io.emit('draw', data);
-    });
+    socket.on('client-ready', () => {
+        socket.broadcast.emit('get-canvas-state')
+      })
+
+      socket.on('canvas-state', (state) => {
+        console.log('received canvas state')
+        socket.broadcast.emit('canvas-state-from-server', state)
+      })
+
+
+
+    socket.on('draw-line', ({ prevPoint, currentPoint, color }) => {
+        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
+      })
+    socket.on('clear', () => io.emit('clear'))
 
     socket.on('disconnect', () => {
       console.log('User is disconnected');
     });
+    
   });
 };
 

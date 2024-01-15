4d15b84e00157b7e73e40649f8fdd29e91b79382
23kartik <kartik23900@gmail.com>
2024-01-15 23:50:54 +0530

    new changes to controller

--- controllers/drawingController.js
+++ controllers/drawingController.js
@@ -1,4 +1,5 @@
 const socketIO = require('socket.io');
+const connectedUsers = new Map();
 
 const initializeDrawing = (server) => {
   const io = socketIO(server, {
@@ -7,9 +8,18 @@ const initializeDrawing = (server) => {
       methods: ['GET', 'POST'],
     },
   });
-
   io.on('connection', (socket) => {
     console.log('A user connected');
+ 
+
+    socket.on('new-users', (userEmail) => {
+      console.log('New user connected:', userEmail);
+      // Add the user to the list of connected users
+      connectedUsers.set(socket.id, userEmail);
+      // Emit the list of connected users to all clients
+      io.emit('update-users', Array.from(connectedUsers.values()));
+        });
+
 
     socket.on('client-ready', () => {
         socket.broadcast.emit('get-canvas-state')
@@ -22,15 +32,21 @@ const initializeDrawing = (server) => {
 
 
 
-    socket.on('draw-line', ({ prevPoint, currentPoint, ctx, color, lineWidth }) => {
-        socket.broadcast.emit('draw-line', {prevPoint, currentPoint, ctx, color, lineWidth  })
-      })
+socket.on('draw-line', ({ prevPoint, currentPoint, ctx, color, lineWidth  }) => {
+    socket.broadcast.emit('draw-line', { prevPoint, currentPoint, ctx, color, lineWidth });
+});
     socket.on('clear', () => io.emit('clear'))
 
     socket.on('disconnect', () => {
-      console.log('User is disconnected');
+      console.log('User disconnected:', connectedUsers[socket.id]);
+      connectedUsers.delete(socket.id);
+
+      // Emit the updated list of connected users to all clients
+      io.emit('update-users', Array.from(connectedUsers.values()));
     });
-    
+    if (socket.id in connectedUsers) {
+      socket.emit('update-users', Array.from(connectedUsers.values()));
+    }
   });
 };
 
